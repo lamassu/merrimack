f77a45c45001d70bebc8a0a3977f84b067de4d7e
Josh Harvey <josh@lamassu.is>
2014-12-13 01:27:44 -0500

    cleanup directory code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Bluebird = require('bluebird');
+var join = Bluebird.join;
 var ByteBuffer = require('bytebuffer');
 var fdb = require('fdb').apiVersion(300);
 var db = fdb.open();
@@ -9,53 +11,38 @@ var RANDOM_FACTOR = 0x1 << RANDOM_BITS;
 var ZERO_64 = new ByteBuffer(8, ByteBuffer.LITTLE_ENDIAN).writeUint64(0).buffer;
 
 var lcounter = 0;
-var directories = {
-  merrimack: null,
-  gcounter: null,
-  records: null,
-  topics: null,
-  timestamps: null
-};
+var dirs;
 
 exports.init = function init() {
-  var merrimack;
   return fdb.directory.createOrOpen(db, 'merrimack')
-  .then(function(_merrimack) {
-    merrimack = _merrimack;
-    return merrimack.createOrOpen(db, 'records');
-  })
-  .then(function(records) {
-    directories.records = records;
-    return merrimack.createOrOpen(db, 'topics');
-  })
-  .then(function(topics) {
-    directories.topics = topics;
-    return merrimack.createOrOpen(db, 'gcounter');
-  })
-  .then(function(gcounter) {
-    directories.gcounter = gcounter;
-    return merrimack.createOrOpen(db, 'timestamps');
-  })
-  .then(function(timestamps) {
-    directories.timestamps = timestamps;
-    return db.max(directories.gcounter, ZERO_64);
+  .then(function(merrimack) {
+    return Bluebird.props({
+      merrimack: merrimack,
+      records: merrimack.createOrOpen(db, 'records'),
+      topics: merrimack.createOrOpen(db, 'topics'),
+      timestamps: merrimack.createOrOpen(db, 'timestamps'),
+      gcounter: merrimack.createOrOpen(db, 'gcounter')
+    })
+    .then(function(result) {
+      dirs = result;
+      exports.directories = dirs;
+      return db.max(dirs.gcounter, ZERO_64);
+    });
   });
 };
-
-exports.directories = directories;
 
 exports.insertRec = function insertRec(topic, message) {
   var messageStr = JSON.stringify(message);
   var now = Date.now();
 
   function fdbInsertRec(tr, cb) {
-    tr.get(directories.gcounter)
+    tr.get(dirs.gcounter)
     .then(function(gcounterBuf) {
       var gcounter = unpackNum(gcounterBuf);
       var rand = Math.floor(Math.random() * RANDOM_FACTOR);
-      var key = directories.records.pack([gcounter, lcounter, rand]);
-      var topicKey = directories.topics.pack([topic, gcounter, lcounter, rand]);
-      var timeKey = directories.timestamps.pack([now, gcounter, lcounter, rand]);
+      var key = dirs.records.pack([gcounter, lcounter, rand]);
+      var topicKey = dirs.topics.pack([topic, gcounter, lcounter, rand]);
+      var timeKey = dirs.timestamps.pack([now, gcounter, lcounter, rand]);
       lcounter++;
       tr.set(key, messageStr);
       tr.set(topicKey, messageStr);
@@ -72,13 +59,13 @@ exports.insertRec = function insertRec(topic, message) {
 
 exports.incrementGlobalCounter = function incrementGlobalCounter() {
   function fdbIncrement(tr, cb) {
-    var r = directories.records.range();
+    var r = dirs.records.range();
     tr.snapshot.getKey(fdb.KeySelector.lastLessThan(r.end))
     .then(function(key) {
-      var gcounter = directories.records.unpack(key)[0] + 1;
+      var gcounter = dirs.records.unpack(key)[0] + 1;
       var gcounterBuf = new ByteBuffer(8, ByteBuffer.LITTLE_ENDIAN).
         writeUint64(gcounter).buffer;
-      tr.max(directories.gcounter, gcounterBuf);
+      tr.max(dirs.gcounter, gcounterBuf);
       cb();
     })
     .catch(function(err) {
