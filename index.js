93b18396f4bcea93af68856669c1241b3b2a8117
Josh Harvey <josh@lamassu.is>
2014-12-16 00:27:08 -0500

    add message layer; fix group bug; need more testing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 // var P = require('bluebird');
+var os = require('os');
 var _ = require('lodash');
 var Joi = require('joi');
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 var fasterror = require('fasterror');
 
 var db = require('./lib/fdb.js');
@@ -13,14 +14,24 @@ var ParamError = fasterror('ParamError');
 var DEFAULT_TTL = 30 * 1000;
 var DEFAULT_TIMEOUT = 10 * 1000;
 
+var GENERATORS = {
+  event: eventMessage,
+  requests: requestMessage,
+  error: errorMessage
+};
+
+var MODES = _.keys(GENERATORS);
+
 var eventSchema = {
-  id: Joi.string.guid(),
+  id: Joi.string().guid(),
   topic: Joi.string().alphanum().max(10),
   mode: Joi.string().alphanum().max(10),
   host: Joi.string().hostname(),
   payload: Joi.object().optional(),
   timestamp: Joi.date().iso()
 };
+
+var eventValidator = Joi.object().keys(eventSchema);
 
 var errorValidator = Joi.object().keys(_.merge({}, eventSchema, {
   mode: 'error',
@@ -37,9 +48,15 @@ var requestValidator = Joi.object().keys(_.merge({}, eventSchema, {
   ttl: Joi.number().integer().greater(0).max(60 * 60 * 1000)
 }));
 
+var modeValidator = Joi.string().valid(MODES);
+
 function validate(obj, validator) {
-  var joi = Joi.validate(validator, {presence: 'required'});
-  if (joi.error) throw new ParamError(joi.error);
+  var joi = Joi.validate(obj, validator, {presence: 'required'});
+  if (joi.error) {
+    var err = new ParamError(joi.error);
+    console.log(err.stack);
+    throw err;
+  }
 }
 
 function generateBaseMessage() {
@@ -52,8 +69,13 @@ function generateBaseMessage() {
 
 function buildMessage(base, opts, validator) {
   var msg = _.merge(base, opts, generateBaseMessage());
-  validate(msg, requestValidator);
+  validate(msg, validator);
   return msg;
+}
+
+function eventMessage(opts) {
+  var base = {mode: 'event'};
+  return buildMessage(base, opts, eventValidator);
 }
 
 function requestMessage(opts) {
@@ -70,9 +92,21 @@ function requestMessage(opts) {
 }
 
 function errorMessage(opts) {
-  var base = {
-    mode: 'error'
-  };
+  var base = {mode: 'error'};
   return buildMessage(base, opts, errorValidator);
 }
 
+exports.init = function init() {
+ return db.init();
+};
+
+exports.produce = function produce(opts) {
+  validate(opts.mode, modeValidator);
+  var messageBuilder = GENERATORS[opts.mode];
+  var msg = messageBuilder(opts);
+  return db.produce(msg);
+};
+
+exports.consume = function consume(topic, group) {
+  return db.consume(topic, group);
+};
